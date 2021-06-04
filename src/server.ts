1a2be614ff1a47a7e913f31da6a4dc0f3ec7c603
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-06-03 22:35:21 -0300

    Migrations, ORM, and Controllers

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
-import express, { response } from 'express';
+import "reflect-metadata";
+import express, { response } from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
-app.get("/", (request, response)=>{
-    return response.json({message: "uiui" });
-});
-
-app.post("/", (request, response) =>{
-    return response.json({message: "aai aii"})
-});
-
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, ()=> console.log("Server is running!"))
 
